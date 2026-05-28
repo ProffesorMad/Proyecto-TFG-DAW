de959fc1e813cd06a262f677b15e18b08b9f55be
Pablo Gracia Gálvez <pablgg13@gmail.com>
2026-05-29 00:50:20 +0200

    Fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./storage/framework/views/*.php",
+        "./app/View/Components/**/*.php",
     ],
 
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
+
+    safelist: [
+        'bg-black',
+        'text-white',
+        'flex',
+        'hidden',
+        'block',
+        'grid',
+    ],
 
     plugins: [],
 }
